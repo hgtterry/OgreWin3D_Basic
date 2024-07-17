7f7fda4599d3d576ebd87d53e57724fb8d9ef26f
hgtterry <terryinflanite@gmail.com>
2024-07-17 15:07:45 +0100

    17/07/24 OgreWin3D_Basic :-- Clean up

--- GD64/framework.h
+++ GD64/framework.h
@@ -1,3 +1,18 @@
+/*
+Copyright (c) OgreWin3D_Basic 2024 W.T.Flanigan H.C.Flanigan Inflanite_HGT
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely.
+
+An acknowledgment in the product documentation would be
+appreciated but is not required.
+*/
+
 #pragma once
 
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
