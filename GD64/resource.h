291e0aac162fc597c244ebbd5166100ddbbe312c
hgtterry <terryinflanite@gmail.com>
2024-09-14 14:14:15 +0100

    14/09/24 OgreWin3D_Basic :-- Debugging Material with no Texture

--- GD64/resource.h
+++ GD64/resource.h
@@ -60,6 +60,8 @@
 #define IDD_FILEVIEWER                  169
 #define IDD_PROPS_TEXTURES              170
 #define IDD_PROPS_TEXTUREVIEWER         171
+#define IDB_BITMAP2                     171
+#define IDB_NO_TEXTURE                  171
 #define IDC_CK_SU_FULLSCREEN            1000
 #define IDC_ST_STARTUP                  1001
 #define IDC_CK_SU_FULL3DVIEW            1002
@@ -213,7 +215,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        171
+#define _APS_NEXT_RESOURCE_VALUE        172
 #define _APS_NEXT_COMMAND_VALUE         32803
 #define _APS_NEXT_CONTROL_VALUE         1068
 #define _APS_NEXT_SYMED_VALUE           110
