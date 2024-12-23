611c3f98003134ab49865187eb92abe9e3f453af
hgtterry <terryinflanite@gmail.com>
2024-12-23 20:48:27 +0000

    23/12/24 OgreWin3D_Stage :- Map Editor

--- OgreWin3D_Stage_Source/CL64_Mesh_Manager.h
+++ OgreWin3D_Stage_Source/CL64_Mesh_Manager.h
@@ -28,5 +28,29 @@ class CL64_Mesh_Manager
 public:
 	CL64_Mesh_Manager(void);
 	~CL64_Mesh_Manager(void);
+
+	bool Ogre_To_Mesh_Data(Ogre::Entity* Ogre_Entity);
+	void Get_Ogre_Mesh_Data(Ogre::Entity* Ogre_Entity);
+
+	bool Has_Shared_Vertices;
+
+private:
+
+	bool Convert_To_Mesh_Data(Ogre::Entity* Ogre_Entity);
+
+	void Get_SubPose_MeshInstance(Ogre::MeshPtr mesh,
+		size_t& vertex_count, Ogre::Vector3*& vertices,
+		size_t& index_count, unsigned long*& indices,
+		int SubMesh, Ogre::int16*& BoneIndices);
+
+	bool Get_SubPoseTextureUV(Ogre::MeshPtr mesh, int SubMesh);
+
+	bool Get_SubPoseNormals(Ogre::MeshPtr mesh, size_t& vertex_count, Ogre::Vector3*& Normals,
+		int SubMesh);
+
+	bool GetBoneAssignment(Ogre::MeshPtr mesh, int SubMesh, HWND hDlg);
+
+	std::vector<Ogre::Vector2> MeshTextureCoords;
+
 };
 
