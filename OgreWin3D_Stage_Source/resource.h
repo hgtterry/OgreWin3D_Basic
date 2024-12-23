3783bf209db650ff7962c0b51bd9bf80e602c32f
hgtterry <terryinflanite@gmail.com>
2024-12-23 06:52:18 +0000

    23/12/24 OgreWin3D_Stage :- Map Editor

--- OgreWin3D_Stage_Source/resource.h
+++ OgreWin3D_Stage_Source/resource.h
@@ -94,6 +94,11 @@
 #define IDD_PROPS_PLAYER                206
 #define IDD_PROPS_OVERRIDECOUNTER       207
 #define IDD_MAPEDITOR                   208
+#define IDD_MAP_TOP_LEFT                209
+#define IDD_MAP_TOP_RIGHT               210
+#define IDD_MAP_TOP_BOTTOM_LEFT         211
+#define IDD_MAP_BOTTOM_LEFT             211
+#define IDD_MAP_BOTTOM_RIGHT            212
 #define IDC_CK_SU_FULL3DVIEW            1002
 #define IDC_BT_TDH_DEBUG                1002
 #define IDC_BT_TD_DEBUG_RESETVIEW       1003
@@ -402,7 +407,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        209
+#define _APS_NEXT_RESOURCE_VALUE        210
 #define _APS_NEXT_COMMAND_VALUE         32844
 #define _APS_NEXT_CONTROL_VALUE         1138
 #define _APS_NEXT_SYMED_VALUE           110
