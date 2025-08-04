85f3406a6ce109db26481772f3a4aed34965c90d
hgtterry <terry660@hotmail.co.uk>
2025-08-04 20:55:01 +0100

    OgreWin3D [04-08-25] Preference Lib [ Menu->Options->Settings Open Last File Option ]

--- X_Lib_Preferences/X_Lib_Preferences.cpp
+++ X_Lib_Preferences/X_Lib_Preferences.cpp
@@ -1,10 +1,10 @@
-// preference_Lib.cpp : Defines the functions for the static library.
+// X_Lib_Preferences.cpp : Defines the functions for the static library.
 //
 
 #include "pch.h"
 #include "framework.h"
 
 // TODO: This is an example of a library function
-void fnpreferenceLib()
+void fnXLibPreferences()
 {
 }
