db7427277175cb121896b077d9b9f87b9b4ca2e4
hgtterry <terry660@hotmail.co.uk>
2025-08-04 14:20:15 +0100

    OgreWin3D [04-08-25] Update to Ogre 14.4.0

--- Test_Lib/Test_Lib/framework.h
+++ Test_Lib/Test_Lib/framework.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS
 #endif
@@ -9,8 +7,6 @@
 #pragma warning( disable : 4251 4193 4275 4005 4267 4244)
 #pragma warning( disable : 4305 4311 4302)
 #pragma warning(disable : 4996 4715) // Needs Looking At
-
-//#include "targetver.h"
 
 #include <windows.h>
 #include "btBulletDynamicsCommon.h"
@@ -25,10 +21,6 @@
 
 
 #include "Ogre.h"
-#include "OgreConfigFile.h"
-#include "OgreStringConverter.h"
-#include "OgreException.h"
-#include "OgreTextAreaOverlayElement.h"
 #include "irrKlang.h"
 
 #define GE_FALSE	(0)
