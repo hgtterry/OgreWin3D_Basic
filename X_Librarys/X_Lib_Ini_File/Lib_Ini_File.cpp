e7183908b5fc80b61592943903fe783852877284
hgtterry <terry660@hotmail.co.uk>
2025-08-14 16:48:32 +0100

    OgreWin3D [14-08-25] Lib Ini File

---
+++ X_Librarys/X_Lib_Ini_File/Lib_Ini_File.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "Lib_Ini_File.h"
+
+Lib_Ini_File::Lib_Ini_File(void)
+{
+}
+
+Lib_Ini_File::~Lib_Ini_File(void)
+{
+}
---
+++ X_Librarys/X_Lib_Ini_File/Lib_Ini_File.h
@@ -0,0 +1,8 @@
+#pragma once
+class Lib_Ini_File
+{
+public:
+	Lib_Ini_File(void);
+	~Lib_Ini_File(void);
+};
+
