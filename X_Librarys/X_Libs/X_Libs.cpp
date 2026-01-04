c712c8acef3a3c948c434ffe33c161e157cffa2b
hgtterry <terry660@hotmail.co.uk>
2026-01-04 08:57:46 +0000

    Cheryl Scene Builder [04-01-26] Libs

---
+++ X_Librarys/X_Libs/X_Libs.cpp
@@ -0,0 +1,10 @@
+// X_Libs.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnXLibs()
+{
+}
