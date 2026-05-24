a31f9092f2ae8f17f70cf8638dc4a5f350a8d8c1
hgtterry <terry660@hotmail.co.uk>
2026-05-24 07:49:21 +0100

    Cheryl 3D Modeller / Cheryl 3D Scene Builder [23-05-26]

---
+++ Cheryl_Modeller/TMH_SceneBuilder/Cheryl_3D_Modeller.h
@@ -0,0 +1,118 @@
+/*
+Copyright (c) 2024 - 2026 HGT_Software W.T.Flanigan H.C.Flanigan
+
+Cheryl 3D Modeller
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "CL64_Views_Com.h"
+
+#include "G_Box.h"
+
+#include "CL64_Render_Map.h"
+#include "CL64_Level.h"
+
+#include "Lib_Preference.h"
+
+#include "Lib_Ini_File.h"
+
+#include "CL64_Doc.h"
+#include "CL64_Utilities.h"
+
+#include "CX_Brush.h"
+
+#include "CX_FaceList.h"
+
+#include "CX_Face.h"
+
+#include "BrushTemplate.h"
+
+#include "CX_SelBrushList.h"
+#include "CX_Array.h"
+
+#include "CX_SelFaceList.h"
+#include "Shapes_3D.h"
+
+#include "CL64_WadFile.h"
+#include "CL64_Mesh_Mgr.h"
+#include "CL64_Brush_X.h"
+// ------------------------------
+#include "CL64_Dialogs.h"
+#include "CL64_Properties_Tabs.h"
+#include "CL64_Properties_Templates.h"
+#include "CL64_Properties_Brushes.h"
+#include "CL64_Properties_Textures.h"
+#include "CL64_Properties_Motions.h"
+
+#include "Face_Editor.h"
+
+#include "CLP_3D_Setup.h"
+#include "CL64_Interface.h"
+#include "CL64_Keyboard.h"
+#include "CL64_Scene.h"
+#include "CL64_Ogre3D.h"
+#include "CL64_Top_Tabs.h"
+#include "CL64_File.h"
+#include "CL64_ParseFile.h"
+
+#include "CL64_Export.h"
+#include "CL64_Exporters.h"
+#include "CL64_Exp_Obj.h"
+
+#include "CL64_File_IO.h"
+#include "CL64_ImGui.h"
+#include "CL64_ImGui_Dialogs.h"
+#include "CL64_ImGui_Editor.h"
+#include "CL64_TXL_Editor.h"
+#include "CL64_Textures.h"
+#include "CL64_Resources.h"
+#include "CL64_Picking.h"
+#include "CL64_Camera.h"
+#include "CL64_Entities.h"
+#include "CL64_Motioins.h"
+#include "CL64_Assimp.h"
+
+#include "CL64_PB.h"
+
+#include "CL64_Editor_Control.h"
+
+// Templates Dialogs
+#include "App_Templates.h"
+#include "CreateStaircaseDialog.h"
+
+// Project
+#include "CL64_Gizmos.h"
+#include "CL64_Dimensions.h"
+#include "CL64_Props_Dialogs.h"
+
+#include "Sandbox.h" // for testing routines
+#include "CL64_Importers.h"
+#include "CL64_Model.h"
+#include "CL64_Mesh.h"
+#include "Init_Libs.h"
+
+#include "CL64_Properties_Textures_Com.h"
+
+#include "CL64_View_Top_Left.h"
+#include "CL64_View_Top_Right.h"
+#include "CL64_View_Bottom_Left.h"
+#include "CL64_View_3D.h"
