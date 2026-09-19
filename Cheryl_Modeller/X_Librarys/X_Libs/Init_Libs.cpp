243f10879fa3bfb75dcbdd8410b79ce39cba637f
hgtterry <terry660@hotmail.co.uk>
2026-09-19 08:57:56 +0100

    Cheryl 3D Modeller / Cheryl 3D Scene Builder [ 19-09-26 ] George

--- Cheryl_Modeller/X_Librarys/X_Libs/Init_Libs.cpp
+++ Cheryl_Modeller/X_Librarys/X_Libs/Init_Libs.cpp
@@ -59,7 +59,7 @@ void Init_Libs::Start_Libraries()
 // *************************************************************************
 char* Init_Libs::GetVersion()
 {
-	return (LPSTR)" Init_Libs [ 13-09-26 ] Build T ";
+	return (LPSTR)" Init_Libs [ 19-09-26 ] Build T ";
 }
 
 // *************************************************************************
--- X_Librarys/X_Libs/Init_Libs.cpp
+++ X_Librarys/X_Libs/Init_Libs.cpp
@@ -33,6 +33,6 @@ void Init_Libs::Start_Libraries()
 // *************************************************************************
 char* Init_Libs::GetVersion()
 {
-	return (LPSTR)" Init_Libs [ 13-09-26 ] Build T ";
+	return (LPSTR)" Init_Libs [ 19-09-26 ] Build T ";
 }
 
